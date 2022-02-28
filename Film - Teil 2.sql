a483e9c59a7a509c799b9c6ea3cc1a920e65ac37
rizemusic <90315479+rizemusic@users.noreply.github.com>
2022-02-28 21:18:26 +0100

    Add files via upload

---
+++ Film - Teil 2.sql
@@ -0,0 +1,41 @@
+-- Teil 2
+/*
+CREATE SCHEMA if not exists filme;
+
+USE filme;
+
+CREATE TABLE Film (
+FilmNr int PRIMARY KEY AUTO_INCREMENT,
+Filmtitel varchar(255),
+Erscheinungsdatum date,
+FirmenNr int
+);
+
+ALTER TABLE Film AUTO_INCREMENT = 3000;
+
+CREATE TABLE Studio (
+StudioID int,
+StudioName varchar(255)
+)
+*/
+
+
+-- Datenbank erweitern
+
+USE filme;
+
+CREATE TABLE Schauspieler (
+SId int PRIMARY KEY,
+Vorname varchar(255),
+Nachname varchar(255),
+Herkunftsland varchar(255),
+FilmNr int,
+CONSTRAINT FK_FilmNr 
+FOREIGN KEY (FilmNr) REFERENCES film(FilmNr)
+);
+
+-- SId in Tabelle erstellen und als Foreign key anlegen
+ALTER TABLE film ADD SId int;
+ALTER TABLE film ADD FOREIGN KEY (SId) REFERENCES Schauspieler(SId)
+
+
